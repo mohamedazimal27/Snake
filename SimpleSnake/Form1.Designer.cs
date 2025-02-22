c871dc576e0dd4189471ba7e740ffacbdc8b6430
Mohamed Azimal <mohamedazimal27@gmail.com>
2025-02-22 18:57:46 +0530

    Initial commit

---
+++ SimpleSnake/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace SimpleSnake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.BackColor = System.Drawing.Color.White;
+            this.lblScore.Location = new System.Drawing.Point(641, 24);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(35, 13);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScore);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ SimpleSnake/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleSnake
+{
+    public partial class Form1 : Form
+    {
+        //Game declarations
+        private List<Point> snake = new List<Point>();
+        private Point food;
+        private int direction = 0;
+        private int score = 0;
+        private Timer gameTimer = new Timer();
+        private Random rand = new Random();
+        private const int tileSize = 20;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(OnKeyPress);
+            this.Paint += new PaintEventHandler(OnPaint);
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            snake.Clear();
+            snake.Add(new Point(15, 5));
+            GenerateFood();
+
+            gameTimer.Interval = 100;
+            gameTimer.Tick += UpdateGame;
+            gameTimer.Start();
+        }
+
+        private void OnKeyPress(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Right:
+                    if (direction != 2) direction = 0;
+                    break;
+                case Keys.Down:
+                    if (direction != 3) direction = 1;
+                    break;
+                case Keys.Left:
+                    if (direction != 0) direction = 2;
+                    break;
+                case Keys.Up:
+                    if (direction != 1) direction = 3;
+                    break;
+            }
+        }
+
+        private void OnPaint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Brush snakecolor = Brushes.Green;
+            Brush foodcolor = Brushes.Red;
+
+            foreach(Point p in snake)
+            {
+                g.FillRectangle(snakecolor, p.X * tileSize, p.Y * tileSize, tileSize, tileSize);
+            }
+
+            g.FillRectangle(foodcolor, food.X * tileSize, food.Y * tileSize, tileSize, tileSize);
+        }
+
+        private void GenerateFood()
+        {
+            int maxX = this.ClientSize.Width / tileSize;
+            int maxY = this.ClientSize.Height / tileSize;
+            food = new Point(rand.Next(maxX), rand.Next(maxY));
+        }
+
+        private void UpdateGame(object sender, EventArgs e)
+        {
+            //Move snake
+            Point newHead = snake[0];
+
+            switch(direction)
+            {
+                case 0: newHead.X += 1; break;
+                case 1: newHead.Y += 1; break;
+                case 2: newHead.X -= 1; break;
+                case 3: newHead.Y -= 1; break;
+            }
+
+            //Collision check (body and wall)
+            if (newHead.X < 0 || newHead.Y < 0 ||
+                newHead.X >= this.ClientSize.Width / tileSize ||
+                newHead.Y >= this.ClientSize.Height / tileSize ||
+                snake.Contains(newHead))
+            {
+                gameTimer.Stop();
+                MessageBox.Show("Game Over! Score: " + score);
+                //StartGame();
+                return;
+            }
+
+            //Check food is eaten
+            if (newHead == food)
+            {
+                score += 10;
+                GenerateFood();
+            }
+            else
+            {
+                snake.RemoveAt(snake.Count - 1);
+            }
+
+            snake.Insert(0, newHead);
+            lblScore.Text = "Score: " + score;
+            this.Invalidate();
+        }
+    }
+}
